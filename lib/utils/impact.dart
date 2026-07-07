3b4a3e909dd0d31889b430edc580f67c125ee371
lauraazzinnaro <lauraazzinnaro1504@gmail.com>
2026-07-07 21:37:22 +0200

    tabella analisi

--- lib/utils/impact.dart
+++ lib/utils/impact.dart
@@ -5,6 +5,14 @@ class ImpactConfig {
   static const String pingEndpoint = 'gate/v1/ping/';
   static const String tokenEndpoint = 'gate/v1/token/';
   static const String refreshEndpoint = 'gate/v1/refresh/';
+
+  static String username = '5UJpUCxIUn';
+  static String password = '12345678!';
+  static String patientUsername = 'IL_PAZIENTE_ASSOCIATO';
+
+  static String heartrateEndpoint = 'data/v1/heartrate/patients/';
+  static String sleepEndpoint = 'data/v1/sleep/patients/';
+  
 }
 
 // Same values used in lesson_18-authentication/impact_authentication.
